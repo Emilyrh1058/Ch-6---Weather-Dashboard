4ee25aa53fbc14f11cbcad14e4010afff02d617d
Emily Salinas <emilyrh1058@gmail.com>
2021-03-28 23:08:23 -0500

    added js for weather info

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
 var searchBtn = document.querySelector("#searchBtn");
 var cityForecast = [];
+var apiKey = "b8b015eeb24126e70a9d70de8dada8aa";
+
 
 // API //
-var getUserCity = function(city) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast" + city + ""
-
-    fetch(apiUrl).then(function(reponse) {
-        if (response.ok) {
-            response.json().then(function(data) {
-               displayForecast(data, city);
-            });
-        } else {
-            alert("Error: " + response.statusText)
-        }
-
-    })
-
+var getLocation = function(city) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // display city OR you can set city to a global variable
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            getWeather(lat, lon);
+        })
 }
 
-
-var getCity = function(event) {
-event.preventDefault();
-var userCity = document.getElementById("searchQuery").value.trim;
-if (userCity) {
-    getUserRepos(username);
-    repoContainerEl.textContent = "";
-    nameInputEl.value = "";
-} else {
-    alert("Please enter a valid city");
+var getWeather = function(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // call a function that takes in data and then displays it on the page
+            weatherForecastSection(data);
+        })
 }
+
+var weatherForecastSection = function(data) {
+    
 };
 
-
-
-console.log(data)
-cityForecast = data;
-
-
-
-searchBtn.addEventListener("click", getCity)
+searchBtn.addEventListener("click", getLocation);
