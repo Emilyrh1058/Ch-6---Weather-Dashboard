e3cdb0ef747d87f59ae2468f235391047f7bfc7a
Emily Salinas <emilyrh1058@gmail.com>
2021-03-28 23:50:24 -0500

    add btn handlers

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 var searchBtn = document.querySelector("#searchBtn");
+var searchFormEl = document.querySelector("#leftCol");
+var searchQueryEl = document.querySelector("#searchQuery");
 var cityForecast = [];
 var apiKey = "b8b015eeb24126e70a9d70de8dada8aa";
+// vars/things to add
+    // icons 
+    // localStorage
+    // display cards 
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityName = cityEl.value.trim();
+    if (cityName) {
+        getLocation(cityName);
+    } else {
+        alert("Please enter a valid city name.")
+    }
+}
+
+var searchListHandler = function(event) {
+    event.preventDefault();
+    var lastSearch = event.target;
+    if (lastSearch.classList.contains("last-city")) { // will link to search history el at the bottom
+        
+        // CREATE THESE BELOW // REVISIT LOCAL STORAGE INFO
+        // storage = lastCity.innerHTML.split(",")[0];
+        // getLocation(storage)
+    }
+}
 
 
-// API //
 var getLocation = function(city) {
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
     fetch(apiUrl)
@@ -26,12 +52,22 @@ var getWeather = function(lat, lon) {
         })
         .then(function(data) {
             // call a function that takes in data and then displays it on the page
-            weatherForecastSection(data);
+            weatherForecast(data);
         })
 }
 
-var weatherForecastSection = function(data) {
-    
-};
+var weatherForecast = function(data) {
 
-searchBtn.addEventListener("click", getLocation);
+};
+// EVENT LISTENERS //
+//searchBtn.addEventListener("click", getLocation);
+
+searchFormEl.addEventListener("click", formSubmitHandler);
+
+
+/////
+const cityInputEl = document.querySelector("#city");
+///
+searchHistoryEl.addEventListener("click", previousSearchHandler);
+getCities();
+
