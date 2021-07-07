4013cd4f487bc55daa2d4666529135f7fb01b7f8
Emily Salinas <emilyrh1058@gmail.com>
2021-07-06 22:38:10 -0500

    can't get the cards to show

--- assets/script.js
+++ assets/script.js
@@ -22,11 +22,6 @@ function getWeather(zipcode) {
             var weatherIcon = document.querySelector("#weather-icon")
             weatherIcon.setAttribute("src", 'http://openweathermap.org/img/wn/'+response.weather[0].icon+'.png');
 
-            var temp = document.querySelector("#current-temp")
-            temp.textContent = "Temperature: " + response.main.temp + "°F";
-            var feelsLike = document.querySelector("#feels-like")
-            feelsLike.textContent = "Feels Like: " + response.main.feels_like + "°F";
-
             var humidity = document.querySelector("#humidity")
             humidity.textContent = "Humidity: " + response.main.humidity + "%";
 
@@ -247,72 +242,3 @@ clearHistoryBtn.addEventListener("click", clearSearchHistory);
 
 
 loadSearchHistory();
-
-
-
-
-
-
-
-
-
-// SEARCH FORM HANDLER /
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    var cityName = cityEl.value.trim();
-    if (cityName) {
-        getLocation(cityName);
-    } else {
-        alert("Please enter a valid city name.")
-    }
-}
- // LAST SEARCH FOR LOCAL STORAGE //
-var searchListHandler = function(event) {
-    event.preventDefault();
-    var lastCitySearch = event.target;
-
-    if (lastCitySearch.classList.contains("last-city")) { // will link to search history el at the bottom
-        cityStorage = lastCitySearch.innerHTML.split(",")[0];
-        getLocation(cityStorage)
-    }
-}
-
-// API KEY AND FETCH //
-var getLocation = function(city) {
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
-    fetch(apiUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            // display city OR you can set city to a global variable
-            let lat = data[0].lat;
-            let lon = data[0].lon;
-            getWeather(lat, lon);
-        })
-}
-
-// DISPLAY WEATHER //
-var getWeather = function(lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            return response.json().then(function(data) {
-                displayWeatherForcast(data);
-            });
-        }
-    });
-}
-
-var weatherForecast = function(data) {
-
-};
-
-// EVENT LISTENERS //
-//searchBtn.addEventListener("click", getLocation);
-
-searchFormEl.addEventListener("click", formSubmitHandler);
-
-
-
-
